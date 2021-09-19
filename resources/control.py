c73a7441ef7c4add7ac77963c5bbbf7f191e5808
Alexandr Mironov <mironov.a.s@outlook.com>
2021-09-19 23:51:42 +0400

    update

--- resources/control.py
+++ resources/control.py
@@ -5,11 +5,12 @@ import xbmcgui
 class TVControl(xbmcgui.Window):
     def __init__(self):
         self.btnOff = xbmcgui.ControlButton(350, 500, 80, 30, "TV OFF")
+        self.btnOn = xbmcgui.ControlButton(350, 500, 80, 30, "TV ON")
 
     def onControl(self, control):
         if control == self.btnOff:
-            xbmc.executebuiltin('CECToggleState')
-        if control == self.button1:
-            self.message('you pushed the 2nd button')
-        if control == self.button2:
-            self.message('you pushed the 3rd button')
+            if xbmc.Player().isPlaying():
+                xbmc.executebuiltin("PlayerControl(Stop)")
+            xbmc.executebuiltin('CECStandby')
+        if control == self.btnOn:
+            xbmc.executebuiltin('CECActivateSource')
