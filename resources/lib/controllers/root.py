0578fdbeab6b912e9d8fefe34918809e2737b4a1
Alexandr Mironov <mironov.a.s@outlook.com>
2021-09-30 22:32:09 +0400

    update

--- resources/lib/controllers/root.py
+++ resources/lib/controllers/root.py
@@ -3,20 +3,23 @@
 def index(router):
     handle = router.session.handle
 
-    item_list = []
+    item_list = [
+        (router.actions_url('on'), xbmcgui.ListItem(label='ON'), False),
+        (router.actions_url('off'), xbmcgui.ListItem(label='OFF'), False)
+    ]
 
-    li = xbmcgui.ListItem(label=mov['title'], label2=mov['description'])
-    li.setArt({'poster': api.art_url(mov['poster_id'])})
-    if mov['fanart_id']:
-        li.setArt({'fanart': api.art_url(mov['fanart_id'], 630, 354)})
-    li.setInfo('video', dict(
-        title=mov['title'],
-        plot=mov['description']
-    ))
-    li.setProperty('IsPlayable', 'true')
-    url = router.root_url('play', id=mov['id'], hls_id=mov['hls_id'])
-    item_list.append((url, li, False))
+    # li = xbmcgui.ListItem(label=mov['title'], label2=mov['description'])
+    # li.setArt({'poster': api.art_url(mov['poster_id'])})
+    # if mov['fanart_id']:
+    #     li.setArt({'fanart': api.art_url(mov['fanart_id'], 630, 354)})
+    # li.setInfo('video', dict(
+    #     title=mov['title'],
+    #     plot=mov['description']
+    # ))
+    # # li.setProperty('IsPlayable', 'true')
+    # url = router.root_url('play', id=mov['id'], hls_id=mov['hls_id'])
+    # item_list.append((url, li, False))
 
-    xbmcplugin.addDirectoryItems(handle, movies, len(movies))
+    xbmcplugin.addDirectoryItems(handle, item_list, len(item_list))
     xbmcplugin.endOfDirectory(handle)
     xbmcplugin.addSortMethod(handle, xbmcplugin.SORT_METHOD_TITLE)
