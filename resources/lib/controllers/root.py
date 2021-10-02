2e619433e6c8a9bf7c24182d3afa948b913e6352
Alexandr Mironov <mironov.a.s@outlook.com>
2021-10-02 23:41:35 +0400

    update

--- resources/lib/controllers/root.py
+++ resources/lib/controllers/root.py
@@ -12,18 +12,6 @@ def index(router, _params=None):
         (router.actions_url('off'), xbmcgui.ListItem(label='OFF'), False)
     ]
 
-    # li = xbmcgui.ListItem(label=mov['title'], label2=mov['description'])
-    # li.setArt({'poster': api.art_url(mov['poster_id'])})
-    # if mov['fanart_id']:
-    #     li.setArt({'fanart': api.art_url(mov['fanart_id'], 630, 354)})
-    # li.setInfo('video', dict(
-    #     title=mov['title'],
-    #     plot=mov['description']
-    # ))
-    # # li.setProperty('IsPlayable', 'true')
-    # url = router.root_url('play', id=mov['id'], hls_id=mov['hls_id'])
-    # item_list.append((url, li, False))
-
     xbmcplugin.addDirectoryItems(handle, item_list, len(item_list))
     xbmcplugin.endOfDirectory(handle)
     xbmcplugin.addSortMethod(handle, xbmcplugin.SORT_METHOD_TITLE)
